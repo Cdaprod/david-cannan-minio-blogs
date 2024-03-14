620f556d82e2eef8e83e74c76775fb638ddb8c36
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2024-03-14 16:05:15 -0400

    Update article link selector in fetch_and_parse_articles function

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def fetch_and_parse_articles():
         summary = article.select_one('div.post__content > p').text.strip() if article.select_one('div.post__content > p') else ''
         date = article.find('time').text.strip() if article.find('time') else ''
         
-        article_link = article.select_one('a.post__more')
+        article_link = article.select_one('a.post__img')
         link = 'https://blog.min.io' + article_link['href'] if article_link else ''
         
         articles.append((title, author, summary, date, link))
